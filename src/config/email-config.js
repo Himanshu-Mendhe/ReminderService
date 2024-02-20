8f62c85212bd3c7d093cbb5fa77ecae4c624924a
Himanshu-Mendhe <himanshu.afcwp23@sinhgad.edu>
2024-02-21 01:03:07 +0530

    smtp setup using nodemailer done;(conputerised email sending)

---
+++ src/config/email-config.js
@@ -0,0 +1,17 @@
+const nodemailer = require('nodemailer');
+const {EMAIL_ID, EMAIL_PASS} = require('./server-config');
+
+console.log(EMAIL_ID, EMAIL_PASS)
+
+const sender = nodemailer.createTransport({
+    service: 'Gmail',
+    host: "smtp.gmail.net",
+    port: 465,
+    secure: true,
+    auth: {
+        user: EMAIL_ID,
+        pass: EMAIL_PASS
+    }
+})
+
+module.exports = sender; 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const app = express();
+
 const {PORT} = require('./config/server-config');
 
+const {sendBasicEmail} = require('./services/email-services')
+
 const SetupAndStartServer = () => { 
+    const app = express();
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended:true}));
 
     app.listen(PORT, () => {
         console.log(`server started on port __${PORT}__`);
+        
+        sendBasicEmail(
+            '"Support"<support@admin.com>',
+            'mendhehimanshu20@gmail.com',
+            'this is a testing email',
+            'This is testing of email npm package. Hopefully it reached you'
+        );
+        
     });
 }
 
