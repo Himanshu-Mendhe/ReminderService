d9cac79fe79be4639e423a59f41aebcb528f011c
Himanshu-Mendhe <himanshu.afcwp23@sinhgad.edu>
2024-02-21 12:38:00 +0530

    added notification ticket model

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ const bodyParser = require('body-parser');
 
 const {PORT} = require('./config/server-config');
 
-const {sendBasicEmail} = require('./services/email-services')
+const cron = require('node-cron');
+//const {sendBasicEmail} = require('./services/email-services')
 
 const SetupAndStartServer = () => { 
     const app = express();
@@ -12,14 +13,18 @@ const SetupAndStartServer = () => {
 
     app.listen(PORT, () => {
         console.log(`server started on port __${PORT}__`);
-        
+        /*
         sendBasicEmail(
             '"Support"<support@admin.com>',
             'mendhehimanshu20@gmail.com',
             'this is a testing email',
             'This is testing of email npm package. Hopefully it reached you'
+            
         );
-        
+        */
+        cron.schedule('*/2 * * * *', () => {
+            console.log('running a task every two minutes');
+          })
     });
 }
 
